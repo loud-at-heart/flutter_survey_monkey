29d4bcc139bbb10385c80481f83f56a4165b9d9f
Krishnakumar Ramachandran <45334931+msarkrish@users.noreply.github.com>
2022-05-25 23:33:05 +0530

    SurveyMonkey library added in build.gradle and opening surveymonkey ui with hashvalue function added and returing value from native android to dart added

--- android/src/main/kotlin/com/muthuselviapps/flutter_survey_monkey/FlutterSurveyMonkeyPlugin.kt
+++ android/src/main/kotlin/com/muthuselviapps/flutter_survey_monkey/FlutterSurveyMonkeyPlugin.kt
@@ -1,20 +1,33 @@
 package com.muthuselviapps.flutter_survey_monkey
 
+import android.app.Activity
+import android.content.Intent
 import androidx.annotation.NonNull
+import com.surveymonkey.surveymonkeyandroidsdk.SurveyMonkey
+import io.flutter.embedding.android.FlutterActivity
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry
 
 /** FlutterSurveyMonkeyPlugin */
-class FlutterSurveyMonkeyPlugin: FlutterPlugin, MethodCallHandler {
+class FlutterSurveyMonkeyPlugin: FlutterPlugin, MethodCallHandler,ActivityAware,PluginRegistry.ActivityResultListener {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
+  val surveyMonkey: SurveyMonkey= SurveyMonkey()
+  private var activity: Activity? = null
+  private lateinit var result: Result
+
+
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_survey_monkey")
@@ -23,7 +36,8 @@ class FlutterSurveyMonkeyPlugin: FlutterPlugin, MethodCallHandler {
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     if (call.method == "openSurvey") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+      this.result=result
+      openSurvey(call.arguments.toString())
     } else {
       result.notImplemented()
     }
@@ -31,5 +45,32 @@ class FlutterSurveyMonkeyPlugin: FlutterPlugin, MethodCallHandler {
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
+  }
+
+  fun openSurvey(surveyHash: String)
+  {
+    surveyMonkey.startSMFeedbackActivityForResult(activity,0,surveyHash)
+  }
+
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    activity = binding.getActivity();
+    binding.addActivityResultListener(this);
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+    activity=null
+  }
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    activity=binding.activity as FlutterActivity
+  }
+
+  override fun onDetachedFromActivity() {
+    activity=null
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
+    result.success("")
+    return true
   }
 }
